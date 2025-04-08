3fdff8a0350dbbea2e4086a1921c8e8e23288c21
Jon <jsmaxi666@gmail.com>
2025-04-08 14:22:55 +0300

    Fix deploy

--- src/actions/init.tsx
+++ src/actions/init.tsx
@@ -7,8 +7,8 @@ import { Server } from "@stellar/stellar-sdk/rpc";
 /* Constants Config */
 const rpcUrl = "https://soroban-testnet.stellar.org";
 const networkPassphrase = "Test SDF Network ; September 2015";
-// const wasmHash =
-//   "bd3c43038d51193306a0d09fff37701a71650ea4efb16b84009241b8dc369ffc";
+const wasmHash =
+  "bd3c43038d51193306a0d09fff37701a71650ea4efb16b84009241b8dc369ffc";
 
 /*
  * Generate a random keypair and fund it
@@ -33,46 +33,48 @@ export async function initMarket(contractID: string): Promise<string> {
       networkPassphrase
     );
 
-    const client: any = await Client.from({
+    const options: any = {
       contractId: contractID,
       networkPassphrase,
       rpcUrl,
-      //wasmHash,
+      wasmHash,
       publicKey: sourceKeypair.publicKey(),
       signTransaction,
-    });
+    };
 
-    const { result: initResult } = await client.init({
+    const client: any = await Client.from(options);
+
+    const res = await client.init({
       user: sourceKeypair.publicKey(),
       data: {
         name: "Nms",
         description: "Desc",
-        admin_address:
-          "GDVZSDU6YDZ53CFP4BMMAXDC4Y3UXOXT3N73MXKK2XPSRBJMZXZ5ZFV5",
+        admin_address: sourceKeypair.publicKey(),
         asset_address:
           "CBIELTK6YBZJU5UP2WWQEUCYKLPU6AUNZ2BQ4WWFEIE3USCIHMXQDAMA",
         trusted_oracle_name: "Test",
         trusted_oracle_address:
           "GDVZSDU6YDZ53CFP4BMMAXDC4Y3UXOXT3N73MXKK2XPSRBJMZXZ5ZFV5",
         hedge_vault_address:
-          "CCFCK3QGNPWA73XL6JOWY4ZICPRXJBCYBXTFUACE74UQP2YCFNJIV7IY",
+          "CAFZTNZ747ZEBAO242AX5WSPPIA65RDD6DGPEOYOWAS5BOM6TEA36AJZ",
         risk_vault_address:
-          "CCWOTBY2WQ5DSY4J5KPZGYDS63WHR7CWCTTYPDUHQHG3R7WAM7BMRHCA",
+          "CBSX6LLBTI7QBF26QCIIZUPUCGHH3SNWKLPNX7ZMPWMJK77Z2ZRHMLT5",
         commission_fee: 5,
         risk_score: 1,
         is_automatic: true,
-        event_unix_timestamp: 1743873501,
+        event_unix_timestamp: 1746651600,
         lock_period_in_seconds: 600,
         event_threshold_in_seconds: 600,
         unlock_period_in_seconds: 600,
       },
     });
 
-    // const { result: initResult } = await client.status();
+    const sent = await res.signAndSend();
 
-    console.log("Market init result:", initResult);
+    console.log("Market init result:", sent.result);
+    console.log("Market init result:", res);
 
-    return JSON.stringify(initResult);
+    return JSON.stringify(sent.result);
   } catch (e) {
     console.log("Failed to init market:", e);
     return "Error: " + e;
